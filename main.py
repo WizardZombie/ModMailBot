0cfd2b704c8c3d273a4c4b1c8b5175a732d3cc2f
WizardZombie <TheHedgehog28@gmail.com>
2019-02-28 17:07:37 +0000

    Basic Framework
    Lays out basic framework for bot

--- main.py
+++ main.py
@@ -17,3 +17,25 @@ logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+
+bot = discord.Bot(command_prefix='.', description="ModMail Bot. DM to contact Staff")
+
+@bot.event
+async def on_ready():
+	pass
+
+@bot.event
+async def on_message(msg):
+	pass
+
+async def on_command_error(ctx, err):
+	pass
+
+async def on_raw_reaction_add(payload):
+	pass
+
+async def on_raw_reaction_remove(payload):
+	pass
+
+
+bot.run(token)
