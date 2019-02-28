97b1d10875feb6bd82e2d50790b1efa1577258a6
WizardZombie <TheHedgehog28@gmail.com>
2019-02-28 17:17:28 +0000

    Setup
    Sets up checking for moderator and admin

--- utils/checks.py
+++ utils/checks.py
@@ -1,3 +1,20 @@
 from discord.ext import commands
 import discord.utils
 import utils.config as config
+
+def _is_mod_or_admin(ctx):
+	author = ctx.message.author
+	mod_role_id = config.getModRoleID()
+	if mod_role_id in [y.id for y in author.roles]:
+		return True
+	else:
+		return _is_admin_check(ctx)
+
+def _is_admin_check(ctx):
+	return ctx.message.author.guild_permissions.administrator
+
+def isMod():
+	return commands.check(_is_mod_or_admin)
+
+def isAdmin():
+	return commands.check(_is_admin_check)
